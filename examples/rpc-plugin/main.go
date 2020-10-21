73e18051f5fc09fb89712c0092a12d0bfbed7abc
Alif Rachmawadi <arch@subosito.com>
2020-10-21 07:13:29 +0000

    add rpc plugin example

---
+++ examples/rpc-plugin/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/subosito/drafter-go"
+	rc "github.com/subosito/drafter-go/rpc-plugin/client"
+)
+
+/**
+Usage:
+
+1. Download file drafter-rpc-* from releases page
+2. Rename it as `drafter-rpc`
+3. Add executable permission (chmod +x drafter.so)
+4. Place it in the registered PATH directory
+5. Run this file: `go run ./main.go`
+*/
+func main() {
+	client := rc.New(rc.Config{})
+	defer client.Close()
+
+	engine, err := client.Dispense()
+	if err != nil {
+		log.Fatalln(err)
+		os.Exit(1)
+	}
+
+	input := strings.NewReader("# API")
+	options := drafter.Options{
+		Format: drafter.JSON,
+	}
+
+	b, _ := engine.Parse(input, options)
+	fmt.Printf("OUTPUT: %s\n", b)
+}
